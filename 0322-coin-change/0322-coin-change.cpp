f7424f3d79ba09deae41eec10b50bce116cb131a
KALINGI KISHORE <23MH1A4291@acoe.edu.in>
2025-09-09 20:43:20 +0530

    Time: 43 ms (45.71%), Space: 49.4 MB (31.02%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,21 @@
 class Solution {
+private:
+    int fun(int i,int k,vector<int>& coins,vector<vector<int>>& dp){
+        if(i == 0){
+            if(k % coins[0]== 0) return k /coins[0];
+            return 1e9;
+        }
+        if(dp[i][k] != -1) return dp[i][k];
+        int notPick = fun(i-1,k,coins,dp);
+        int pick = 1e9;
+        if(coins[i] <= k) pick = 1 + fun(i,k-coins[i],coins,dp);
+        return dp[i][k] = min(pick,notPick);
+    }
 public:
-    int coinChange(vector<int>& coins, int k) {
+    int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<int> prev(k+1,0),cur(k+1,0);
-        for(int i=0;i<=k;i++){
-            if(i%coins[0] == 0) prev[i] = i / coins[0];
-            else prev[i] = 1e9;
-        }
-        for(int i=1;i<n;i++){
-            for(int tar=0;tar<=k;tar++){
-                int notPick = prev[tar];
-                int pick  = 1e9;
-                if(coins[i] <= tar) pick = 1 + cur[tar-coins[i]];
-                cur[tar] = min(pick,notPick);
-            }
-            prev= cur;
-        }
-        int ans = prev[k];
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        int ans = fun(n-1,amount,coins,dp);
         if(ans >= 1e9) return -1;
         return ans;
     }
