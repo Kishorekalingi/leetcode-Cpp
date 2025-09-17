4daca4cc0731a76aa181b033962e16a8d65c803f
KALINGI KISHORE <23MH1A4291@acoe.edu.in>
2025-09-17 10:51:46 +0530

    Time: 0 ms (100%), Space: 17.2 MB (48.18%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,12 +2,15 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int maxPro = 0;
-        int mini = prices[0];
+        int mini  = prices[0];
         for(int i=0;i<prices.size();i++){
-            if(prices[i] > mini) {
-                maxPro += prices[i] - mini;
+            if(mini < prices[i]){
+                maxPro += prices[i]- mini;
+                mini = prices[i];
             }
-            mini = prices[i];
+            else{
+                mini = min(mini,prices[i]);
+            }
         }
         return maxPro;
     }
