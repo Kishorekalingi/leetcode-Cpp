83b46b6859db3270ca85f5263c29626484c6c84e
KALINGI KISHORE <23MH1A4291@acoe.edu.in>
2025-10-09 21:47:27 +0530

    Time: 0 ms (100%), Space: 14.5 MB (82.44%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,21 +1,25 @@
 class Solution {
-private:
-    int fun(int i,vector<int>& prices,int cap,int b,vector<vector<vector<int>>>&dp){
-        if(i==prices.size() || cap == 0) return 0;
-        if(dp[i][b][cap] != -1) return dp[i][b][cap];
-        int profit = 0;
-        if(!b){
-            profit = max(-prices[i] + fun(i+1,prices,cap,1,dp),fun(i+1,prices,cap,0,dp));
-        }
-        else{
-            profit = max(prices[i] + fun(i+1,prices,cap-1,0,dp),fun(i+1,prices,cap,1,dp));
-        }
-        return dp[i][b][cap] = profit;
-    }
 public:
-    int maxProfit(int k, vector<int>& prices) {
+    int maxProfit(int k,vector<int>& prices) {
+        vector<vector<int>> after(2,vector<int>(k+1,0));
+        vector<vector<int>> cur(2,vector<int>(k+1,0));
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
-        return fun(0,prices,k,0,dp);
+        for(int i=n-1;i>=0;i--){
+            for(int b =0;b<=1;b++){
+                for(int c = 1;c<=k;c++){
+                    if(!b){
+                        cur[b][c] = max(-prices[i] + after[1][c],after[0][c]);
+                    }
+                    else{ 
+                        cur[b][c] = max(after[1][c], prices[i]+ after[0][c-1]);
+                    }
+                }
+                
+            }
+            after = cur;
+        }
+        
+        return after[0][k];
+
     }
 };
