f1defc262a26152faf47efb71a3604f7ffbe262f
KALINGI KISHORE <23MH1A4291@acoe.edu.in>
2025-10-20 20:04:13 +0530

    Time: 0 ms (100%), Space: 17.6 MB (74.44%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int cnt = 0;
+        for(int i=0;i<operations.size();i++){
+            if(operations[i] == "X++" || operations[i] == "++X"){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+        return cnt;
+    }
+};
